283e895342094dee09c7b822097f38feabfdf491
alphaoumardiallo <alphaoumardev@outlook.com>
2023-05-21 22:58:37 +0800

    tiktok tags update

--- insta/profiles.py
+++ insta/profiles.py
@@ -16,45 +16,50 @@ driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 driver.refresh()
 
-with open('cate.csv', mode='r', newline='', encoding='utf-8') as reading, open('profiles.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('ins.csv', mode='r', newline='', encoding='utf-8') as reading, open('profiles.csv', mode='w', newline='',
+                                                                              encoding='utf-8') as file:
     reader = csv.reader(reading)
     writer = csv.writer(file)
     writer.writerow(['Tags', 'Platform', 'Avatar', 'Profile Name', 'Profile Url', 'Location', 'Followers Count',
-                     'Likes', 'Description', 'Email', '3 Latest Videos Url', '3 Latest Videos Title',
+                     'Category', 'Description', 'Email', '3 Latest Videos Url', '3 Latest Videos Title',
                      '3 Latest videos image'])
 
-    driver.get("https://www.tiktok.com/")
-    time.sleep(15)
-    driver.get("https://www.tiktok.com/@alx.suarez2/video/7227602923918740779")
-    time.sleep(20)
+    driver.get("https://www.instagram.com/")
+    time.sleep(10)
     driver.refresh()
 
     for row in reader:
         try:
-            driver.get(row[1])  # //*[@id="search_user-item-user-link-0"]/a[2]/p[1]
-            tag = row[0]
-            platform = 'Tiktok'
+            driver.get(
+                'https://www.instagram.com/{}'.format(row[0]))  # //*[@id="search_user-item-user-link-0"]/a[2]/p[1]
+            tag = row[2]
+            platform = 'Instagram'
             try:
-                avatar = driver.find_element(By.XPATH, value='//*[@id="main-content-others_homepage"]/div/div[1]/div[1]/div[1]/span/img').get_attribute(
+                avatar = driver.find_element(By.XPATH,
+                                             value='//*[@id="mount_0_0_AF"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/div/div/span/img').get_attribute(
                     'src')
             except:
                 avatar = ''
             try:
-                name = driver.find_element(by="xpath", value='//*[@id="main-content-others_homepage"]/div/div[1]/div[1]/div[2]/h2').text
+                name = driver.find_element(by="xpath",
+                                           value='//*[@id="mount_0_0_AF"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/a/h2').text
             except:
                 name = ''
             profile_url = driver.current_url
-            location = "USA"
+            location = row[4]
             try:
-                followers = driver.find_element(by="xpath", value='//*[@id="main-content-others_homepage"]/div/div[1]/h3/div[2]/strong').text + " Followers"
+                followers = driver.find_element(by="xpath",
+                                                value='//*[@id="mount_0_0_AF"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a/span/span').text + " Followers"
             except:
                 followers = '0 Flollower'
             try:
-                likes = driver.find_element(by="xpath", value='//*[@id="main-content-others_homepage"]/div/div[1]/h3/div[3]/strong').text + ' Likes'
+                cat = driver.find_element(by="xpath",
+                                          value='//*[@id="mount_0_0_AF"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[3]/div[2]/div').text
             except:
-                likes = '0 Like'
+                cat = '0 Like'
             try:
-                description = driver.find_element(by="xpath", value='//*[@id="main-content-others_homepage"]/div/div[1]/h2').text
+                description = driver.find_element(by="xpath",
+                                                  value='//*[@id="mount_0_0_AF"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[3]/h1').text
             except:
                 description = ''
             try:
@@ -62,15 +67,21 @@ with open('cate.csv', mode='r', newline='', encoding='utf-8') as reading, open('
                 email = driver.find_element(by='xpath', value='//*[@id="email"]').get_attribute('href')
             except:
                 email = 'N/A'
+
+            # reels ? Posts
             try:
                 for i in range(1, 3):
-                    videos_url = driver.find_elements(by='xpath', value='//*[@id="main-content-others_homepage"]/div/div[2]/div[2]/div/div[{}]/div[2]/a'.format(i))
+                    videos_url = driver.find_elements(by='xpath',
+                                                      value='//*[@id="main-content-others_homepage"]/div/div[2]/div[2]/div/div[{}]/div[2]/a'.format(
+                                                          i))
                     video_url = [link.get_attribute('href') for link in videos_url[:3]]
             except:
                 video_url = []
             try:
                 for i in range(1, 3):
-                    videos_title = driver.find_elements(by='xpath', value='//*[@id="main-content-others_homepage"]/div/div[2]/div[2]/div/div[{}]/div[2]/a'.format(i))
+                    videos_title = driver.find_elements(by='xpath',
+                                                        value='//*[@id="main-content-others_homepage"]/div/div[2]/div[2]/div/div[{}]/div[2]/a'.format(
+                                                            i))
                     video_title = [title.get_attribute('title') for title in videos_title[:3]]
             except:
                 video_title = []
@@ -91,7 +102,7 @@ with open('cate.csv', mode='r', newline='', encoding='utf-8') as reading, open('
 
             # Write the data to the CSV file
             writer.writerow(
-                [tag, platform, avatar, name, profile_url, location, followers, likes, description, email, video_url,
+                [tag, platform, avatar, name, profile_url, location, followers, cat, description, email, video_url,
                  video_title, video_image, other_link])
         except:
             print(row)
